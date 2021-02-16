a07a27fd2f3db92239673aa71963e314af8dbd54
GGolfz <wisarutgolf11@gmail.com>
2021-02-16 20:56:45 +0700

    [ADD] 0136

---
+++ 0136-single-number.js
@@ -0,0 +1,14 @@
+var singleNumber = function(nums) {
+    for(let i = 0 ; i<nums.length;i++){
+        let c = 0;
+        for(let j = 0; j<nums.length;j++){
+            if(nums[i] == nums[j]){
+                c++;
+            }
+        }
+        if(c == 1){
+            return nums[i];
+        }
+    }
+    return 0;
+};
