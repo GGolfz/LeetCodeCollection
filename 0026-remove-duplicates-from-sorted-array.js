a3f04d7e126b56846d616012cc933061633a1d11
GGolfz <wisarutgolf11@gmail.com>
2021-03-17 00:26:49 +0700

    [ADD] 0026

---
+++ 0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,12 @@
+var removeDuplicates = function(nums) {
+    let prev = nums[0]
+    for(let i = 1 ; i < nums.length ; i++){
+        if(nums[i] === prev) {
+            nums.splice(i,1);
+            i--;
+        } else {
+            prev = nums[i]; 
+        }  
+    }
+    return nums.length;
+};
