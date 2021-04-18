5e95cf6016b2c53e625ccc13ee2948186975c4f6
GGolfz <wisarutgolf11@gmail.com>
2021-04-18 19:40:19 +0700

    [ADD] 1342

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,13 @@
+var numberOfSteps = function(num) {
+    let count = 0;
+    while(num != 0){
+        count += 1;
+        if(num%2 == 0) {
+            num /=2
+        }
+        else {
+            num-=1
+        }
+    }
+    return count;
+};
