b970a17383ced2536e1a92a816d56e354298562c
GGolfz <wisarutgolf11@gmail.com>
2021-09-09 20:53:34 +0700

    [ADD] 1991

---
+++ 1991-find-middle-index-array.js
@@ -0,0 +1,12 @@
+var findMiddleIndex = function(nums) {
+    const sum = (arr) => arr.length >= 0 && arr.reduce((prev,cur) => prev+cur,0);
+    for(let i = 0 ; i<nums.length ;i++) {
+        let left =nums.slice(0,i);
+        let right = nums.slice(i+1,nums.length)
+        if(sum(left) == sum(right)){
+            return i
+        }
+    }
+    return -1;
+};
+
