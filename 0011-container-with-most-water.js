aa3159018f36ab24a74acb54527deb749de64c66
GGolfz <wisarutgolf11@gmail.com>
2021-09-28 19:15:56 +0700

    [ADD] 11

---
+++ 0011-container-with-most-water.js
@@ -0,0 +1,15 @@
+var maxArea = function(height) {
+    let start = 0;
+    let end = height.length - 1;
+    let max = 0;
+    while(start < end) {
+        max = Math.max((end-start) * Math.min(height[start],height[end]),max);
+        if(height[start] <= height[end]){
+            start++
+        }
+        else{
+            end--
+        }
+    }
+    return max;
+};
