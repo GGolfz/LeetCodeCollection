9cbde42f8a35780c8ee390c244f7c93dced158ac
GGolfz <wisarutgolf11@gmail.com>
2021-10-23 20:57:17 +0700

    [ADD] 33

---
+++ 0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,14 @@
+var search = function(nums, target) {
+    let first = nums[0];
+    let last = nums[nums.length - 1];
+    if(target >= first) {
+        for(let i = 0 ; i < nums.length;i++){
+            if(target == nums[i]) return i;
+        }
+    } else {
+        for(let i = nums.length - 1 ; i >= 0 ;i--){
+            if(target == nums[i]) return i;
+        }
+    }
+    return -1
+};
