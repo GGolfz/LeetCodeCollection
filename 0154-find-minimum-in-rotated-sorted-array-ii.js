6e7525bf39d7bbcd90668bfc9c7a80b66f64cfb0
GGolfz <wisarutgolf11@gmail.com>
2021-10-23 21:03:06 +0700

    [ADD] 154

---
+++ 0154-find-minimum-in-rotated-sorted-array-ii.js
@@ -0,0 +1,25 @@
+var findMin = function(nums) {
+    let first = nums[0];
+    let last = nums[nums.length - 1];
+    let min = 5001;
+    if(last <= first) {
+        min = last;
+        for(let i = nums.length - 1 ; i >= 0 ; i--) {
+            if(nums[i] <= min) {
+                min = nums[i];
+            } else {
+                return min;
+            }
+        }
+    } else {
+        min = first;
+        for(let i = 0 ; i < nums.length ; i++) {
+            if(nums[i] <= min) {
+                min = nums[i];
+            } else {
+                return min;
+            }
+        }
+    }
+    return min;
+};
